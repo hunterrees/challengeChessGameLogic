e045f63f05907d60fd54b469a2984809312db597
Brian Tiu <Brian@android-7c594f3a6cf70ed9.app.byu.edu>
2017-02-06 17:22:27 -0700

    game engine and computer player initial push

---
+++ ComputerPlayer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class ComputerPlayer {
+public:
+  ComputerPlayer(int difficulty) {}
+
+  int getBestMove(uint16_t board[128]) {
+    return -1;
+  }
+  
+private:
+};
---
+++ GameEngine.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class GameEngine {
+public:
+  GameEngine() {}
+  
+  vector<int> getValidMoves() {
+    return NULL;
+  }
+
+  int updateGameState(int loc1, int loc2, int updateStatus, int promotionPiece) {
+    return -1;
+  }
+
+  vector<int> getGameBoard() {
+    return NULL;
+  }
+
+
+private:
+};
